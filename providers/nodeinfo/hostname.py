4109155d17a7265a7cf0076330d2a5e69e461ab9
Byggvir <thomas@arend-rhb.de>
2020-06-22 23:09:20 +0200

    Modified hostname

--- providers/nodeinfo/hostname.py
+++ providers/nodeinfo/hostname.py
@@ -3,6 +3,11 @@ import socket
 
 class Source(providers.DataSource):
     def required_args(self):
-        return ['batadv_dev']
-    def call(self, batadv_dev):
-        return socket.gethostname() + '-' + batadv_dev[3:]
+        return ['batadv_dev', 'domain_code', 'known_codes']
+
+    def call(self, batadv_dev, domain_code, known_codes):
+        try:
+            return socket.gethostname() + '-' + known_codes[batadv_dev]
+        except KeyError:
+            return socket.gethostname() + '-' + domain_code
+
